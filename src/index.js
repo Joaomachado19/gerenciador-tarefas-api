2af879ebc6c001cc04cbf61164a1a691fd44bf4d
Joaomachado19 <joaovwmachado@gmail.com>
2025-04-24 11:42:55 -0300

    #adicionado html basico para demonstração da API

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const routes = require('./routes');
 const swaggerConfig = require('./docs/swagger');
+const path = require('path');
 
 dotenv.config();
 
@@ -10,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/api', routes);
-
+app.use(express.static(path.join(__dirname, 'public')));
 swaggerConfig(app);
 
 app.listen(PORT, () => {
